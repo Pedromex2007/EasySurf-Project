6e669ca7b0c5c09510dae6b0545d7ff833faf38a
Pedromex2007 <34046361+Pedromex2007@users.noreply.github.com>
2022-03-12 18:58:34 -0500

    Make account info more verbose in admin page

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from account.models import Account
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
-admin.site.register(Account)
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'username', 'date_joined', 'first_name', 'last_name', 'is_admin', 'is_staff')
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    readonly_fields = ('date_joined', 'last_login')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Account, AccountAdmin)
