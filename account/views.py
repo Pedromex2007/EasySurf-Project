98b707b93838ffcaea1113a6af0c1e2bec777bf2
Pedromex2007 <34046361+Pedromex2007@users.noreply.github.com>
2022-03-14 11:55:24 -0400

    Add 'personal-info.html' to view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
 
-# Create your views here.
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+def update_account_info(request):
+    context = {}
+    return render(request, "account/personal-info.html", context=context)
--- easysurfHome/urls.py
+++ easysurfHome/urls.py
@@ -4,10 +4,12 @@ from django.urls import path, include
 from .views import HomePageView
 
 from account.views import (
+    update_account_info,
     logout_view,
 )
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='easysurf-home'),
     path('logout', logout_view, name='logout'),
+    path('updateinfo', update_account_info, name='update-info'),
 ]
