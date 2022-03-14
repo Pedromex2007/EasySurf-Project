023b3642b3094613d42485bc773f26c8cfbe8c07
Pedromex2007 <34046361+Pedromex2007@users.noreply.github.com>
2022-03-14 11:58:29 -0400

    Add csrf_token to personal-info.html template

--- easysurf/urls.py
+++ easysurf/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from account.views import (
-    logout_view,
-)
 
 # easysurfHome will be the front page of the site.
 urlpatterns = [
