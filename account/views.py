cb6d4add9c9ba1cdcd6490ec787947169e87cdfd
Pedromex2007 <34046361+Pedromex2007@users.noreply.github.com>
2022-03-14 17:32:07 -0400

    Add simple login functionality

--- account/views.py
+++ account/views.py
@@ -1,13 +1,38 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
+from account.forms import AccountAuthenticationForm
 
 def logout_view(request):
     logout(request)
-    return redirect('home')
+    return redirect('easysurf-home')
+
+def login_view(request):
+    context = {}
+    user = request.user
+    if(user.is_authenticated):
+        return redirect('easysurf-home')
+    if request.POST:
+        form = AccountAuthenticationForm(request.POST)
+        if form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+
+            if(user):
+                login(request, user)
+                return redirect('easysurf-home')
+    else:
+        form = AccountAuthenticationForm()
+    context['login_form'] = form
+    return render(request, 'account/login.html', context)
+
 
 def update_account_info(request):
-    context = {}
-    if request.method == "POST":
-        fname = request.POST.get("fname")
-        print(fname)
-    return render(request, "account/personal-info.html", context=context)
+    if(request.user.is_authenticated):
+        context = {}
+        if request.method == "POST":
+            fname = request.POST.get("fname")
+            print(fname)
+        return render(request, "account/personal-info.html", context=context)
+    else:
+        return redirect('easysurf-home') 
--- easysurfHome/urls.py
+++ easysurfHome/urls.py
@@ -6,10 +6,12 @@ from .views import HomePageView
 from account.views import (
     update_account_info,
     logout_view,
+    login_view,
 )
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='easysurf-home'),
     path('logout', logout_view, name='logout'),
+    path('login', login_view, name='login'),
     path('updateinfo', update_account_info, name='update-info'),
 ]
