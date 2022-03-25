d59c3f0d5d441b3d69e270e7ad9ccb8c90e48885
Pedromex2007 <34046361+Pedromex2007@users.noreply.github.com>
2022-03-25 14:49:11 -0400

    Stylize the login page

--- account/forms.py
+++ account/forms.py
@@ -3,19 +3,30 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate
 from account.models import Account
 
+
 class AccountAuthenticationForm(forms.ModelForm):
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    '''This form will render and create the login fields.'''
+
+    #password = forms.CharField(label='Password', widget=forms.PasswordInput)
 
     class Meta:
         model = Account
         fields = ('email', 'password')
+
+        widgets = {
+            'email': forms.TextInput(attrs={'class': 'form-control'}),
+            'password': forms.TextInput(attrs={
+                'class': 'form-control',
+                'type': 'password',
+                }),
+        }
 
     def clean(self):
         if self.is_valid():
             email = self.cleaned_data['email']
             password = self.cleaned_data['password']
             if not authenticate(email=email, password=password):
-                print("ERROR")
+                print("ERROR INVALID LOGIN")
                 raise forms.ValidationError("Invalid login.")
 
 class AccountUpdateForm(forms.ModelForm):
