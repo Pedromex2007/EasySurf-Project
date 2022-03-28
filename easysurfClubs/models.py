bd3e20da519144c494e662095fd651bac337e1ee
Pedromex2007 <34046361+Pedromex2007@users.noreply.github.com>
2022-03-28 11:20:10 -0400

    Add club model

--- easysurfClubs/models.py
+++ easysurfClubs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Club(models.Model):
+    '''Model for clubs/committees (we'll just call them clubs in the code.) Users can only be apart of one club in order to make this easier due to limited time.'''
+    name = models.CharField(max_length=50)
+    desc = models.CharField(max_length=500)    
+    
+    def __str__(self):
+        return self.name
