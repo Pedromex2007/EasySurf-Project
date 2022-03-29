f11abfdc6d347ab55fee3e26bf9a76e42025df85
Pedromex2007 <34046361+Pedromex2007@users.noreply.github.com>
2022-03-29 14:34:03 -0400

    Add community issue database models

--- easysurfCommunityIssues/models.py
+++ easysurfCommunityIssues/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from account.models import Account
+import random
 
-# Create your models here.
+class Issue(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=500)
+    upvotes = models.IntegerField(default=0)
+    downvotes = models.IntegerField(default=0)
+
+    def get_upvotes(self):
+        return self.upvotes
+
+    def get_downvotes(self):
+        return self.downvotes
+
+class Reply(models.Model):
+    content = models.CharField(max_length=200)
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.content
+
+class Voter(models.Model):
+    user = models.ForeignKey(Account)
+    issue = models.ForeignKey(Issue)
--- easysurfHome/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-#Note that user account models will be held in this application
-#This app must be imported in other apps in order to access user accounts.
-
-
