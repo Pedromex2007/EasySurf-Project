7243f2f5207f79b4848f3f13e747ed3c2b25a0d9
Pedromex2007 <34046361+Pedromex2007@users.noreply.github.com>
2022-03-31 20:57:14 -0400

    Add event model

--- easysurfCommunityIssues/models.py
+++ easysurfCommunityIssues/models.py
@@ -1,17 +1,22 @@
-from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
 from account.models import Account
+from django.utils import timezone
 import random
 
 class Issue(models.Model):
     title = models.CharField(max_length=50)
     content = models.CharField(max_length=500)
 
+    date_posted = models.DateTimeField(default=timezone.now)
+
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
 
     upvotes = models.IntegerField(default=0)
     downvotes = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['date_posted']
 
     def get_upvotes(self):
         return self.upvotes
@@ -23,6 +28,8 @@ class IssueReply(models.Model):
     content = models.CharField(max_length=500)
     issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    date_posted = models.DateTimeField(default=timezone.now)
     
     def __str__(self):
         return self.content
--- easysurfEvents/models.py
+++ easysurfEvents/models.py
@@ -1,3 +1,15 @@
+from asyncio.windows_events import NULL
 from django.db import models
+from django.contrib.auth.models import User
+from account.models import Account
+from django.utils import timezone
+import random
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=500)
+    date_posted = models.DateTimeField(default=timezone.now)
+    event_date = models.DateTimeField(default=NULL, blank=True, null=True)
+
+    poster = models.ForeignKey(Account, on_delete=models.CASCADE)
+    subscribers = models.ManyToManyField(Account)
