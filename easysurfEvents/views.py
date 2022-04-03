add97c4276dc14f46e6bd755772e86dd59db27d9
Pedromex2007 <34046361+Pedromex2007@users.noreply.github.com>
2022-04-03 14:39:27 -0400

    Add basic event HTML template

--- easysurfEvents/views.py
+++ easysurfEvents/views.py
@@ -12,7 +12,7 @@ from .models import Event
 from django.http import HttpResponseRedirect
 
 class EventListView(LoginRequiredMixin, ListView):
-    '''This view lists out all the issues all users have posted. Users can click on an issue to see all the responses and also agree/disagree with the post.'''
+    '''This view lists out all the active events.'''
     model = Event
     login_url = 'easysurf-home'
     template_name = 'easysurfEvents/index.html'
@@ -21,22 +21,19 @@ class EventListView(LoginRequiredMixin, ListView):
     def get(self, request, *args, **kwargs):
         self.object_list = self.get_queryset()
         context = self.get_context_data()
-        print("loo")
         return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
         self.object_list = self.get_queryset()
         context = self.get_context_data()
-        print("poo")
         return render(request, self.template_name, context)
 
 class EventDetailView(LoginRequiredMixin, DetailView):
-    '''View to render a specific issue's replies and upvotes/downvotes. This also controls the logic behind the reply form and the downvote/upvote button.'''
+    '''View to render an event's details.'''
     model = Event
     login_url = 'easysurf-home'
 
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
         context = self.get_context_data(object=self.object)
-        print("Getting!")
         return self.render_to_response(context)
