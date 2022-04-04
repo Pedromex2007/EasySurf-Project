aff898375cfbef339fc43af9f538b93134438426
Pedromex2007 <34046361+Pedromex2007@users.noreply.github.com>
2022-04-04 14:03:12 -0400

    Allow users to subscribe to event

--- easysurfEvents/urls.py
+++ easysurfEvents/urls.py
@@ -4,4 +4,5 @@ from .views import EventListView, EventDetailView
 
 urlpatterns = [
     path('', EventListView.as_view(), name='event-list'),
+    path('<int:pk>/', EventDetailView.as_view(), name='event-eventdetail'),
 ]
