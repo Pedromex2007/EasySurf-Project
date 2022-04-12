e7561717721ed625c5599bd6c6e2896640ff71c4
Pedromex2007 <34046361+Pedromex2007@users.noreply.github.com>
2022-04-12 19:41:00 -0400

    Integrate Dashboard

--- easysurfCommunityIssues/urls.py
+++ easysurfCommunityIssues/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import IssueCreateView, IssueDetailView, IssueListView, IssueEditView
 
 urlpatterns = [
-    path('create/', IssueCreateView.as_view(), name='issues-list'),
+    path('create/', IssueCreateView.as_view(), name='issues-list-create'),
     path('', IssueListView.as_view(), name='issues-list'),
     path('<int:pk>/', IssueDetailView.as_view(), name='issues-issuedetail'),
     path('edit/<int:pk>/', IssueEditView.as_view(), name='update-issue'),
