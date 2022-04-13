f342e96e8c2563cd121ffa062f6cd05105c69a61
Pedromex2007 <34046361+Pedromex2007@users.noreply.github.com>
2022-04-13 11:20:48 -0400

    Finalize Database Models

--- easysurfClubs/models.py
+++ easysurfClubs/models.py
@@ -3,7 +3,10 @@ from django.db import models
 class Club(models.Model):
     '''Model for clubs/committees (we'll just call them clubs in the code.) Users can only be apart of one club in order to make this easier due to limited time.'''
     name = models.CharField(max_length=50)
-    desc = models.CharField(max_length=500)    
+    desc = models.CharField(max_length=500)   
+
+    club_portrait = models.ImageField(default=None, null=True, upload_to="images/")
+
     members = models.IntegerField(default=0)
     
     def __str__(self):
--- easysurfHome/models.py
+++ easysurfHome/models.py
@@ -2,11 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 from account.models import Account
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 class Visitor(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     phone_number = models.CharField(max_length=12)
+
+    class Relationship(models.TextChoices):
+        SON = 'SO', _('Son')
+        DAUGHTER = 'DA', _('Daughter')
+        GRANDSON = 'GS', _('Grandson')
+        GRANDDAUGHTER = 'GD', _('Granddaughter')
+        BROTHER = 'BR', _('Brother')
+        SISTER = 'SI', _('Sister')
+
+    visitor_relationship = models.CharField(
+        max_length=2,
+        choices=Relationship.choices,
+        default=Relationship.SON,
+    )
 
     visitor_portrait = models.ImageField(default=None, null=True, upload_to="images/")
 
