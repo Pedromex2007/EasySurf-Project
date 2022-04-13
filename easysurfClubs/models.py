7452ed6cc10b5696272be3e679c5a8d8b83659a2
Pedromex2007 <34046361+Pedromex2007@users.noreply.github.com>
2022-04-13 15:56:49 -0400

    Add location and meet times to club model

--- easysurfClubs/models.py
+++ easysurfClubs/models.py
@@ -4,6 +4,8 @@ class Club(models.Model):
     '''Model for clubs/committees (we'll just call them clubs in the code.) Users can only be apart of one club in order to make this easier due to limited time.'''
     name = models.CharField(max_length=50)
     desc = models.CharField(max_length=500)   
+    location = models.CharField(max_length=50)
+    meet_times = models.CharField(max_length=50)
 
     club_portrait = models.ImageField(default=None, null=True, upload_to="images/")
 
